dc223604b428ab25ede8cdf09df738e499af9d0e
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2024-12-26 10:39:00 -0500

    Update extract_seq.sh

--- extract_seq.sh
+++ extract_seq.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-genome_file=*genomic.fna #change to actual file name
 exec 3< pkhd1_variants.vcf
 while IFS= read -r line <&3; do
-    if [[ "$line" =~ ^# ]]; then
-        continue
-    fi
-    chromosome="chromosome "$(echo "$line" | awk '{print $1}') #store the chromosome number
-    echo "Storing $chromosome sequence..."
-    sequence=$(grep -A 1 "$chromosome " $genome_file) #match the chromosome number to the genome file
+        if [[ "$line" =~ ^# ]]; then
+                continue
+        fi
+
+        chromosome="chromosome "$(echo "$line" | awk '{print $1}')
+
+        echo "Storing $chromosome sequence..."
+        chr1=$(grep -n -m 1 "$chromosome " GCF_000001405.40_GRCh38.p14_genomic.fna|cut -d: -f1)
+        echo "chr1: "$chr1
+        nxt_chrom="chromosome "$(echo "$chromosome" | cut -d' ' -f2|awk '{print$1+1}')
+        echo "nxt_chrom: "$nxt_chrom
+        chr2=$(grep -n -m 1 "$nxt_chrom " GCF_000001405.40_GRCh38.p14_genomic.fna|cut -d: -f1)
+        echo "chr2: "$chr2
+        num=$((chr2-chr1))
+        echo "num: "$num
+        sequence=$(grep -A $num "$chromosome " GCF_000001405.40_GRCh38.p14_genomic.fna)
+        new_chrom="${chromosome// /}"
+        echo $sequence > "$new_chrom.txt"
 done
 
 exec 3<&-
+
