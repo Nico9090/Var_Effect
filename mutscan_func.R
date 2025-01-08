d38ce03d525d25569b1bea60d25cd2b3a8a6277b
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-01-08 15:58:30 -0500

    Update mutscan_func.R

--- mutscan_func.R
+++ mutscan_func.R
@@ -5,27 +5,49 @@ library(rentrez)
 #Protein: PKHD1
 
 #Functions======================================================================
-get_mrNA_seq<-function(gene_name){
-  #'Obtain the mrNA sequence of a protein
-  #FROM CHATGPT
-  search_results <- entrez_search(db="nucleotide", term=gene_name)
-  mRNA_id <- search_results$ids[1]
-  mRNA_sequence <- entrez_fetch(db="nucleotide", id=mRNA_id, rettype="gb", retmode="text")
-  mRNA_seq<-sub(paste0(".*", "ORIGIN", "\\s*"), "", mRNA_sequence)
-  return(mRNA_seq)
+#Use Rentrez to obtain mRNA sequence
+#*******************************************************************************
+mRNA_get<-function(gene){
+
+  #Source code for using Rentrez: CHATGPT
+  res<- entrez_search(db="nucleotide", 
+                      term="Homo sapiens[Species] AND PKHD1[Gene]")
+  
+  file("RNA_seq.txt","w")
+  writeLines(entrez_fetch(db="nucleotide", 
+                          id=res$ids[1],#first id matches search term 
+                          rettype="gb", 
+                          retmode="text"),"RNA_seq.txt")
+  #mRNA_seq<-sub(paste0(".*", "ORIGIN", "\\s*"), 
+  #              "", 
+  #              mRNA_sequence)
+  return()
 
 }
-writeLines(paste("Read", line_num), fastq_file)
+#*******************************************************************************
 make_fastq<-function(gene_mRNA,fastq_file_name){
-  seq<-strsplit(gene_mRNA,"\n")
+  seq<-unlist(strsplit(gene_mRNA,"\n"))
+  
+  
   fastq_file<-file(fastq_file_name,"w")
+  
+  
+  line_num<-1
+  
+  
   for(line in seq){
-    line_num<-1
-    writeLines(paste("Read ",line_num),fastq_file)
+    
+    writeLines(paste("@Read ",line_num),fastq_file)
+    
     writeLines(line,fastq_file)
+    
     writeLines("+",fastq_file)
-    writeLines(paste(rep(40, nchar(length(line)))),fastq_file)
-    line_num=line_num+1
+    
+    quality<-paste(rep("40", nchar(line)), collapse = "")
+    
+    writeLines(quality, fastq_file)
+    
+    line_num<-line_num+1
   }
   
   close(fastq_file)
@@ -35,3 +57,4 @@ make_fastq<-function(gene_mRNA,fastq_file_name){
 pkhd1_mrna<-get_mrNA_seq("PKHD1")
 make_fastq(pkhd1_mrna,"pkhd1_mrna.fastq")
 print(pkhd1_mrna_fastq)
+
