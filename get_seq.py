2acb2da84a739a337de56e3f11df6abd259e0dd8
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-01-09 16:08:40 -0500

    Update get_seq.py

--- get_seq.py
+++ get_seq.py
@@ -5,7 +5,7 @@ import pandas as pd
 df=pd.read_csv("VUS_and_scores.csv")
 df["Gene"]=df["Gene"]+"_HUMAN" # format for gene:"LAMB2_HUMAN"
 def search_uniprot_ids(query):
-        Entrez.email = ""
+        Entrez.email = "jonick9090@gmail.com"
         handle = Entrez.esearch(db="protein", term=query, retmax=10)  # Search for top 10 results
         record = Entrez.read(handle)
         try:
@@ -17,13 +17,19 @@ def search_uniprot_ids(query):
 gene_ids=[]
 for i in range(len(df)):
         gene_ids.append(search_uniprot_ids(df.iloc[i]["Gene"]))
-print(gene_ids)
 
-#def get_protein_sequence(accession):
-#    Entrez.email = ""
-#    handle = Entrez.efetch(db="protein", id=accession, rettype="gb", retmode="text")
-#    record = SeqIO.read(handle, "genbank")
-#    return record.seq
+def get_protein_sequence(accession):
+    Entrez.email = "jonick9090@gmail.com"
+    handle = Entrez.efetch(db="protein", id=accession, rettype="gb", retmode="text")
+    record = SeqIO.read(handle, "genbank")
+    return record.seq
 
-#with open((gene_human_name+".fasta"),'w')as fasta_file:
-#       fasta_file.write(str(get_protein_sequence(gene_id)))
+for i in range(len(gene_ids)):
+        fasta_file=open((df.iloc[i]["Gene"]+".fasta"),'w')
+        try:
+                seq=get_protein_sequence(gene_ids[i])
+        except TypeError:
+                continue
+        else:
+                print(str(seq),file=fasta_file)
+        fasta_file.close()
