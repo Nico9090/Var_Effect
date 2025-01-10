029587e778d26aa8ca3052cce2368d8cb1f0c1ac
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-01-10 13:38:29 -0500

    Update AA_seq_compare.py

--- Structure_prediction/AA_seq_compare.py
+++ Structure_prediction/AA_seq_compare.py
@@ -1,1 +1,3 @@
-
+#From Google AI
+#AlphaFold2 compares the amino acid sequence of the target protein to similar sequences from other proteins. 
+#This helps identify parts of the sequence that are more likely to mutate
