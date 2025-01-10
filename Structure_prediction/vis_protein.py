ea8c4b01265fffdd8374ab2267857fa68b495ebf
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-01-10 13:51:06 -0500

    Update vis_protein.py

--- Structure_prediction/vis_protein.py
+++ Structure_prediction/vis_protein.py
@@ -2,7 +2,7 @@ import nglview as nv
 from Bio import PDB
 
 # Load your protein structure (for example, from a PDB file)
-pdb_file = "C:/Users/teemo/Downloads/fibrocystin_and_L_models/human_fc_parts/hfbr_1_to_500.pdb"
+pdb_file = ""
 
 # Create a PDB parser to load the file
 parser = PDB.PDBParser(QUIET=True)
